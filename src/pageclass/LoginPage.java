7e5389a31e64992286f23a3adef6a61213283d37
prajakta <prajaktawasnik7297@gmail.com>
2024-02-02 14:06:58 +0530

    changes done in login page class

--- src/pageclass/LoginPage.java
+++ src/pageclass/LoginPage.java
@@ -1,5 +1,15 @@
 package pageclass;
 
 public class LoginPage {
+	
+	public void validateTitle()
+	{
+		System.out.println("validating title of login page");
+	}
+	
+	public void enterCredential()
+	{
+		System.out.println("entering credentials");
+	}
 
 }
